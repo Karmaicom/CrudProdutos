f43a0e533019d5c341a0fac361f07090887f8f4e
Karmaicom Rocha <karmaicom@gmail.com>
2026-08-11 17:03:20 -0300

    Criacao da classe de entidade Produto.

---
+++ CrudProdutos/Entities/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace CrudProdutos.Entities
+{
+    /// <summary>
+    /// Modelo de entidade para produto
+    /// </summary>
+    public class Produto
+    {
+        #region Propriedades
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataHoraCadastro { get; set; } = DateTime.Now;
+
+        #endregion
+
+
+    }
+}
---
+++ CrudProdutos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudProdutos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
